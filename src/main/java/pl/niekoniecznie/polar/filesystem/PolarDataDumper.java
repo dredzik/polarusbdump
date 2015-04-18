b96c582f15a065ba8d78c6988f7e4efa3dcb901c
Adam Kuczyński <ak@niekoniecznie.pl>
2015-04-18 14:58:38 +0200

    PolarDataDumper dumps training session directories.

--- src/main/java/pl/niekoniecznie/polar/filesystem/PolarDataDumper.java
+++ src/main/java/pl/niekoniecznie/polar/filesystem/PolarDataDumper.java
@@ -9,6 +9,10 @@ import java.nio.file.*;
 public class PolarDataDumper {
 
     private final static String USER_DIRECTORY_PREFIX = "/U/0/";
+    private final static String DATE_DIRECTORY_REGEXP = "/U/0/\\d{8,}/";
+    private final static String TIME_DIRECTORY_REGEXP = "/U/0/(\\d{8,})/E/(\\d{6,})/";
+    private final static String SAMPLES_FILE_REGEXP = "/U/0/\\d{8,}/E/\\d{6,}/SAMPLES.GZB";
+
     private final Path destination;
 
     public PolarDataDumper(final Path destination) {
@@ -16,14 +20,62 @@ public class PolarDataDumper {
     }
 
     public void dump() throws IOException {
-        dumpDirectory(new PolarFile(USER_DIRECTORY_PREFIX));
+        dumpUserDirectory(new PolarFile(USER_DIRECTORY_PREFIX));
     }
 
-    private void dumpDirectory(final PolarFile directory) throws IOException {
-        String input = directory.getPath();
-        String output = outputPath(input);
+    private void dumpUserDirectory(final PolarFile directory) throws IOException {
+        for (PolarFile child : directory.listFiles()) {
+            if (!child.isDirectory()) {
+                continue;
+            }
 
-        System.out.println("Entering " + input);
+            String path = child.getPath();
+
+            if (!path.matches(DATE_DIRECTORY_REGEXP)) {
+                continue;
+            }
+
+            dumpDateDirectory(child);
+        }
+    }
+
+    private void dumpDateDirectory(final PolarFile directory) throws IOException {
+        PolarFile eDirectory = new PolarFile(directory.getPath() + "E/");
+
+        for (PolarFile child : eDirectory.listFiles()) {
+            if (!child.isDirectory()) {
+                continue;
+            }
+
+            String path = child.getPath();
+
+            if (!path.matches(TIME_DIRECTORY_REGEXP)) {
+                continue;
+            }
+
+            dumpTimeDirectory(child);
+        }
+    }
+
+    private void dumpTimeDirectory(final PolarFile directory) throws IOException {
+        boolean dump = false;
+
+        for (PolarFile child : directory.listFiles()) {
+            String path = child.getPath();
+
+            if (path.matches(SAMPLES_FILE_REGEXP)) {
+                dump = true;
+                break;
+            }
+        }
+
+        if (!dump) {
+            return;
+        }
+
+        String input = directory.getPath();
+        String output = destination.toString();
+        output += input.replaceFirst(TIME_DIRECTORY_REGEXP, "/$1$2/");
 
         try {
             Files.createDirectory(Paths.get(output));
@@ -33,28 +85,14 @@ public class PolarDataDumper {
 
         for (PolarFile child : directory.listFiles()) {
             if (child.isDirectory()) {
-                dumpDirectory(child);
-            } else {
-                dumpFile(child);
+                continue;
             }
+
+            String tmp = output + child.getPath().replaceFirst(TIME_DIRECTORY_REGEXP, "");
+            System.out.println(tmp);
+
+            PolarFileInputStream is = new PolarFileInputStream(child);
+            Files.copy(is, Paths.get(tmp), StandardCopyOption.REPLACE_EXISTING);
         }
-    }
-
-    private void dumpFile(final PolarFile file) throws IOException {
-        String input = file.getPath();
-        String output = outputPath(input);
-
-        System.out.println("Dumping " + input + " into " + output);
-
-        PolarFileInputStream is = new PolarFileInputStream(file);
-        Files.copy(is, Paths.get(output), StandardCopyOption.REPLACE_EXISTING);
-    }
-
-    private String outputPath(String input) {
-        String result = destination.toString();
-
-        result += input.substring(USER_DIRECTORY_PREFIX.length() - 1);
-
-        return result;
     }
 }
