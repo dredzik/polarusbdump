faf820d3b94aa9092d8ced9c96633eccde242e56
Adam Kuczyński <ak@niekoniecznie.pl>
2015-11-18 01:47:10 +0100

    Added garmin tcx and fiddled a little around it.

--- src/main/java/pl/niekoniecznie/ParseSessionAction.java
+++ src/main/java/pl/niekoniecznie/ParseSessionAction.java
@@ -3,6 +3,8 @@ package pl.niekoniecznie;
 import com.codeminders.hidapi.ClassPathLibraryLoader;
 import com.codeminders.hidapi.HIDDevice;
 import com.codeminders.hidapi.HIDManager;
+import com.garmin.xmlschemas.trainingcenterdatabase.v2.*;
+import com.sun.org.apache.xerces.internal.jaxp.datatype.XMLGregorianCalendarImpl;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import pl.niekoniecznie.polar.device.PolarDevice;
@@ -11,7 +13,15 @@ import pl.niekoniecznie.polar.filesystem.PolarLister;
 import pl.niekoniecznie.polar.model.PolarModel;
 import pl.niekoniecznie.polar.service.PolarService;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.util.GregorianCalendar;
 import java.util.zip.GZIPInputStream;
 
 public class ParseSessionAction implements Runnable {
@@ -41,28 +51,63 @@ public class ParseSessionAction implements Runnable {
     public void run() {
         lister.list(POLAR_USER_DIRECTORY, POLAR_SESSION_REGEX).forEach(x -> {
             String source = x.replaceFirst(POLAR_SESSION_REGEX, "$1/");
-            new Thread(() -> parse(source)).start();
+            parse(source);
+            //new Thread(() -> parse(source)).start();
         });
     }
 
     private void parse(String source) {
-        logger.trace("Parsing " + source);
+        logger.trace(source + ": Parsing...");
 
         try {
+            logger.trace(source + ": Parsing physical...");
             PolarModel.PhysicalInformation physical = PolarModel.PhysicalInformation.parseFrom(filesystem.get(source + "PHYSDATA.BPB"));
+            logger.trace(source + ": Parsing session...");
             PolarModel.Session session = PolarModel.Session.parseFrom(filesystem.get(source + "TSESS.BPB"));
+            logger.trace(source + ": Parsing exercise...");
             PolarModel.Exercise exercise = PolarModel.Exercise.parseFrom(filesystem.get(source + "00/BASE.BPB"));
+            logger.trace(source + ": Parsing route...");
             PolarModel.Route route = PolarModel.Route.parseFrom(new GZIPInputStream(filesystem.get(source + "00/ROUTE.GZB")));
+            logger.trace(source + ": Parsing laps...");
             PolarModel.LapInfo laps = PolarModel.LapInfo.parseFrom(filesystem.get(source + "00/LAPS.BPB"));
+            logger.trace(source + ": Parsing alaps...");
             PolarModel.LapInfo alaps = PolarModel.LapInfo.parseFrom(filesystem.get(source + "00/ALAPS.BPB"));
+            logger.trace(source + ": Parsing sample...");
             PolarModel.Sample sample = PolarModel.Sample.parseFrom(new GZIPInputStream(filesystem.get(source + "00/SAMPLES.GZB")));
+            logger.trace(source + ": Parsing statistic...");
             PolarModel.Statistic statistic = PolarModel.Statistic.parseFrom(filesystem.get(source + "00/STATS.BPB"));
+            logger.trace(source + ": Parsing zone...");
             PolarModel.Zone zone = PolarModel.Zone.parseFrom(filesystem.get(source + "00/ZONES.BPB"));
 
-            logger.trace("Parsed " + source);
-        } catch (IOException e) {
+            logger.trace(source + ": Creating TCX...");
+            ObjectFactory factory = new ObjectFactory();
+
+            GregorianCalendar calendar = new GregorianCalendar();
+            calendar.set(session.getStart().getDate().getYear(), session.getStart().getDate().getMonth(), session.getStart().getDate().getDay(), session.getStart().getTime().getHour(), session.getStart().getTime().getMinute(), session.getStart().getTime().getSecond());
+            calendar.set(calendar.MILLISECOND, session.getStart().getTime().getMilisecond());
+
+            TrainingCenterDatabaseT tcx = factory.createTrainingCenterDatabaseT();
+
+            ActivityListT activities = factory.createActivityListT();
+            tcx.setActivities(activities);
+
+            ActivityT activity = factory.createActivityT();
+            activities.getActivity().add(activity);
+
+            activity.setSport(SportT.RUNNING);
+            activity.setId(DatatypeFactory.newInstance().newXMLGregorianCalendar(calendar));
+
+            ActivityLapT lap = factory.createActivityLapT();
+            lap.setStartTime(new XMLGregorianCalendarImpl());
+
+            JAXBContext c = JAXBContext.newInstance(ObjectFactory.class);
+            Marshaller m = c.createMarshaller();
+            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+            m.marshal(factory.createTrainingCenterDatabase(tcx), System.out);
+
+            logger.trace(source + ": TCX created.");
+        } catch (DatatypeConfigurationException | JAXBException | IOException e) {
             throw new RuntimeException(e);
         }
-
     }
 }
